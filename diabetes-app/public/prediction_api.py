d0c769ab2cc86a7b03876dbdb176d1d30efef4b9
AvanFabian <avanfabiandaniswara@gmail.com>
2024-01-12 16:34:49 +0700

    delete halaman prediksi old

--- diabetes-app/public/prediction_api.py
+++ diabetes-app/public/prediction_api.py
@@ -9,15 +9,12 @@ model = joblib.load("storage/model/diabetes3.joblib")
 @app.route('/fitur-diabetes', methods=['POST'])
 def predict():
     data = request.get_json()
-    # print(data)
     input_values = list(map(float, data['input'].values()))
     prediction = model.predict_proba([input_values]).tolist()
-    print(prediction)
     # Mengambil probabilitas kelas positif (misalnya, indeks 1)
     positive_probability = prediction[0][1]
     # Mengubah probabilitas menjadi persen dengan dua desimal
     percentage = "{:.2%}".format(positive_probability)
-    print(f"Percentage chance of diabetes: {percentage}")
     return jsonify({'prediction': percentage})
 
 if __name__ == '__main__':
